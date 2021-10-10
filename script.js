9a3dd97439b42c3d83941ee0735266561e071c59
Oleh Radchenko <89965788+OlehRadchenko@users.noreply.github.com>
2021-10-10 19:53:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,180 @@
+$(document).ready(function(){
+    $('.parallax_lista>li').addClass('layer');
+    $('.parallax_lista').parallax();
+
+    $('.glowna').addClass('active');
+});
+function zadanie1()
+{
+window.open("przyklad1a.html");
+}
+
+function zadanie1_k()
+{
+window.open("przyklad1a_kod.html");
+}
+
+function zadanie2_k()
+{
+window.open("zadanie2_kod.html");
+}
+
+function zadanie2()
+{
+window.open("zadanie2.html");
+}
+
+function zadanie3()
+{
+window.open("zadanie3.html");
+}
+
+function zadanie4()
+{
+window.open("zadanie4.html");
+}
+
+function zadanie5()
+{
+window.open("zadanie5.html");
+}
+
+function zadanie6()
+{
+window.open("zadanie6.html");
+}
+
+function zadanie7()
+{
+window.open("zadanie7.html");
+}
+
+function zadanie8()
+{
+window.open("zadanie8.html");
+}
+
+function zadanie8_k()
+{
+window.open("zadanie8_kod.html");
+}
+
+function zadanie9()
+{
+window.open("zadanie9.html");
+}
+
+function zadanie9_k()
+{
+window.open("zadanie9_kod.html");
+}
+
+function zadanie10()
+{
+window.open("zadanie10.html");
+}
+
+function zadanie10_k()
+{
+window.open("zadanie10_kod.html");
+}
+
+function zadanie11()
+{
+window.open("zadanie11.html");
+}
+
+function zadanie11_k()
+{
+window.open("zadanie11_kod.html");
+}
+
+function zadanie12()
+{
+window.open("zadanie12.html");
+}
+
+function zadanie12_k()
+{
+window.open("zadanie12_kod.html");
+}
+
+function zadanie13()
+{
+window.open("zadanie13.html");
+}
+
+function zadanie13_k()
+{
+window.open("zadanie13_kod.html");
+}
+
+function zadanie14()
+{
+window.open("zadanie14.html");
+}
+
+function zadanie14_k()
+{
+window.open("zadanie14_kod.html");
+}
+
+function zadanie15()
+{
+window.open("zadanie15.html");
+}
+
+function zadanie15_k()
+{
+window.open("zadanie15_kod.html");
+}
+
+function zadanie16()
+{
+window.open("zadanie16.html");
+}
+
+function zadanie17()
+{
+window.open("zadanie17.html");
+}
+
+function zadanie17_k()
+{
+window.open("zadanie17_kod.html");
+}
+
+function zadanie18()
+{
+window.open("zadanie18.html");
+}
+
+function zadanie19()
+{
+window.open("zadanie19.html");
+}
+
+function zadanie20()
+{
+window.open("zadanie20.html");
+}
+
+function zadanie21()
+{
+window.open("zadanie21.html");
+}
+
+function zadanie22()
+{
+window.open("zadanie22.html");
+}
+
+function zadanie23()
+{
+window.open("zadanie23.html");
+}
+
+function zadanie24()
+{
+window.open("zadanie24.html");
+}
